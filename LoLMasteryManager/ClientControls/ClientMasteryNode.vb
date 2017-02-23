825e530b28296d86dd4605f903ae6d26c80bd318
Dewster <office@dewster.hu>
2017-02-23 10:46:05 +0100

    v2
    v2

---
+++ LoLMasteryManager/ClientControls/ClientMasteryNode.vb
@@ -0,0 +1,10 @@
+﻿Partial Public Module ClientControls
+
+    Public Class ClientMasteryNode
+
+        Public Const X As Double = ClientSize.Small.Width / 20
+        Public Const Y As Double = ClientSize.Small.Height / 20
+
+    End Class
+
+End Module
